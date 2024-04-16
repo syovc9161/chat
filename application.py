b02d551a5f35a979591ffef57e9f5fa56b947b93
황기승/데이터인프라부/한국투자증권/인터넷 <112616@truefriend-pc.ad>
2024-04-16 09:41:26 +0900

    'first'

---
+++ application.py
@@ -0,0 +1,83 @@
+# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+# pip3 install faiss-cpu
+# pip3 install -U sentence-transformers
+
+import torch
+import numpy as np
+# import faiss
+import pandas as pd
+
+from sentence_transformers import util
+
+# from sentence_transformers import SentenceTransformer
+# model = SentenceTransformer("Huffon/sentence-klue-roberta-base")
+# torch.save(model, f'./model.pt')
+device = "cuda" if torch.cuda.is_available() else "cpu"
+embedder = torch.load("model.pt", map_location=device)
+
+# em_func = lambda x : embedder.encode(x)
+# em_array = em_func(data['insert_words'].to_numpy())
+# np.save('em.npy', em_array)
+em_array = np.load('em.npy')
+
+data = pd.read_csv('menu.md')
+data['insert_words'] = data[['eat_time', 'sector', 'store', 'menu_type', 'menu', 'remark']].apply(" ".join, axis=1)
+# data.head()
+
+
+query = "오늘 같은날 내일은"
+query_embedding = embedder.encode(query)
+
+docs = len(em_array)
+top_k = min(6, len(em_array))
+cos_scores = util.pytorch_cos_sim(query_embedding, em_array)[0]
+top_results = torch.topk(cos_scores, k=top_k)
+
+# print(f"입력 문장: {query}")
+# print(f"<입력 문장과 유사한 {top_k} 개의 문장>")
+
+store_list = []
+for i, (score, idx) in enumerate(zip(top_results[0], top_results[1])):
+    # print(f"{i+1}: {data.store.to_numpy()[idx]} {'(유사도: {:.4f})'.format(score)}")
+    store_list.append(data.store.to_numpy()[idx])
+store_list = list(set(store_list))
+store_list = ', '.join(store_list)
+
+
+
+from flask import Flask, render_template, request
+import requests
+import sys
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "Hello goorm!" 
+
+def format_response(resp, useCallback=False):
+    data = {
+            "version": "2.0",
+            "useCallback": useCallback,
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": resp
+                        }
+                    }
+                ]
+            }
+        }
+    return data
+
+# executor = ThreadPoolExecutor(max_workers=1)
+
+@application.route('/chat-kakao', methods=['POST'])
+def chat_kakao():
+    print('request.json', request.json)
+    response_to_kakao = format_response('짜장면이나 먹어')
+    return response_to_kakao
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
